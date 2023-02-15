a4ce4b52a3568966ff5abb86d1ee87fe3b96c56c
Thomas <thomsgiacinto@hotmail.it>
2023-02-15 23:28:55 +0100

    Initial Commit

---
+++ Angel/AngelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngelApp.swift
+//  Angel
+//
+//  Created by Thomas Giacinto on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AngelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
